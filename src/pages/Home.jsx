348ebca71f7e8c582bcb84472a7ad87f74192696
Sergey <sergey.potapov.2002@mail.ru>
2022-07-02 11:26:53 +0500

    add FullPost

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,7 @@ export const Home = () => {
   const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
 
   React.useEffect(() => {
     dispatch(fetchPosts());
@@ -35,9 +36,9 @@ export const Home = () => {
               <Post key={index} isLoading={true} />
             ) : (
               <Post
-                id={1}
-                title="Roast the code #1 | Rock Paper Scissors"
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                id={obj._id}
+                title={obj.title}
+                imageUrl={obj.imageUrl}
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
@@ -48,7 +49,7 @@ export const Home = () => {
             ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -39,6 +39,18 @@ const postsSlice = createSlice({
         [fetchPosts.rejected]: (state) => {
             state.posts.items = [];
             state.posts.status = 'error';
+        },
+        [fetchTags.pending]: (state) => {
+            state.tags.items = [];
+            state.tags.status = 'loading';
+        },
+        [fetchTags.fulfilled]: (state, action) => {
+            state.tags.items = action.payload;
+            state.tags.status = 'loaded';
+        },
+        [fetchTags.rejected]: (state) => {
+            state.tags.items = [];
+            state.tags.status = 'error';
         }
     }
 });
